06739f99ffa2e950db8773d2b123ef0f0a95ced0
loveboylion <loveboy_lion@yahoo.com>
2019-03-06 20:21:31 +0530

    tomato: add USB hal
    Change-Id: Iaa08555a7cbc94f17619028b43fced5a042d8e18

--- device.mk
+++ device.mk
@@ -104,6 +104,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml \
     $(LOCAL_PATH)/configs/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# USB HAL
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.0-service.cyanogen_8916
+
 # HIDL
 PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
