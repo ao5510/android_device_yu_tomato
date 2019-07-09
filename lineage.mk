5566a79bd0b57de17a064eb772612f3dfec31971
Lionel <loveboy_lion@yahoo.com>
2019-07-09 15:43:17 +0530

    tomato: Rebrand to LineageOS
    Change-Id: If9f958640bcc7fd645318e8d44168fa629635c9f

--- lineage.mk
+++ lineage.mk
@@ -14,10 +14,10 @@
 
 $(call inherit-product, device/yu/tomato/full_tomato.mk)
 
-# Inherit some common CM stuff.
+# Inherit some common lineage stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_NAME := cm_tomato
+PRODUCT_NAME := lineage_tomato
 BOARD_VENDOR := yu
 PRODUCT_DEVICE := tomato
 
